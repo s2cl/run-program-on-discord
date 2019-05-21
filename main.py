a10aa0625293930535befeaf20dde25c13d1f454
s2cl <yukisunictf@gmail.com>
2019-05-21 13:03:34 +0900

    対応言語を増やした

--- main.py
+++ main.py
@@ -9,16 +9,56 @@ client = commands.Bot(command_prefix='```')
 @client.command()
 async def py(ctx, *, text):
     stdout = run_prog("python3", text.rstrip("```"))
-    await ctx.send(f"```\n{stdout}\n```")
+    await ctx.send(stdout)
 
 @client.command()
 async def python(ctx, *, text):
     stdout = run_prog("python3", text.rstrip("```"))
-    await ctx.send(f"```\n{stdout}\n```")
+    await ctx.send(stdout)
 
 @client.command()
 async def c(ctx, *, text):
     stdout = run_prog("c", text.rstrip("```"))
-    await ctx.send(f"```\n{stdout}\n```")
+    await ctx.send(stdout)
+
+@client.command()
+async def cpp(ctx, *, text):
+    stdout = run_prog("c++", text.rstrip("```"))
+    await ctx.send(stdout)
+
+@client.command()
+async def ml(ctx, *, text):
+    stdout = run_prog("OCaml", text.rstrip("```"))
+    await ctx.send(stdout)
+
+@client.command()
+async def php(ctx, *, text):
+    stdout = run_prog("php", text.rstrip("```"))
+    await ctx.send(stdout)
+
+@client.command()
+async def js(ctx, *, text):
+    stdout = run_prog("JavaScript", text.rstrip("```"))
+    await ctx.send(stdout)
+
+@client.command()
+async def ruby(ctx, *, text):
+    stdout = run_prog("ruby", text.rstrip("```"))
+    await ctx.send(stdout)
+
+@client.command()
+async def rb(ctx, *, text):
+    stdout = run_prog("ruby", text.rstrip("```"))
+    await ctx.send(stdout)
+
+@client.command()
+async def go(ctx, *, text):
+    stdout = run_prog("Go", text.rstrip("```"))
+    await ctx.send(stdout)
+
+@client.command()
+async def rust(ctx, *, text):
+    stdout = run_prog("Rust", text.rstrip("```"))
+    await ctx.send(stdout)
 
 client.run(os.environ["TOKEN"])
